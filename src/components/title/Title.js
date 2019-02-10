add94738f15cd2350c31b782323152398d6986ce
evanpowell <evanpowellmusic@gmail.com>
2019-02-09 19:52:11 -0600

    style title screen

--- src/components/title/Title.js
+++ src/components/title/Title.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import './Title.scss';
+
 const Title = () => {
   return (
     <div className="screen">
-      <h1 className="screen--title">Snake</h1>
+      <h1 className="screen--title main-title">Snake</h1>
       <div className="options">
         <button className="options--btn">Start</button>
         <button className="options--btn">Settings</button>
