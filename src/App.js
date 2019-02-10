db3227c117e594495b88721712e6d46b929d6baa
evanpowell <evanpowellmusic@gmail.com>
2019-02-09 18:40:52 -0600

    implement initial scaffolding

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+import './App.scss';
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+
+  }
+  
+  render() {
+    return (
+      <div className="container">
+        <h1>Hello World!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
