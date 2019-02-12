23502e5874613ff6f9786a2b30b6cb47f3123f76
evanpowell <evanpowellmusic@gmail.com>
2019-02-12 14:15:00 -0600

    refactor server

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.static(path.join(__dirname, '../dist')));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../dist/index.html'));
+  res.render(path.join(__dirname, '../dist/index.html'));
 });
 
 app.listen(PORT, () => {
