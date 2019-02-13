a3774a5dc5d95f8c3f82ae89f88774e875119e53
evanpowell <evanpowellmusic@gmail.com>
2019-02-13 16:41:22 -0600

    refactor server

--- server/index.js
+++ server/index.js
@@ -7,10 +7,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../dist')));
 
-app.get('/*', (req, res) => {
-  res.render(path.join(__dirname, '../dist/index.html'));
-});
-
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
